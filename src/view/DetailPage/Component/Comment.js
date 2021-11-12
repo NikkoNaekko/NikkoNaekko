4bd04ecec5466d6283af1a362137a1c65212c09d
ABlued <53801395+ABlued@users.noreply.github.com>
2021-11-12 22:12:13 +0900

    ##패치노트 - added Na (2021.11.12) 댓글 수정 뷰 구성

--- src/view/DetailPage/Component/Comment.js
+++ src/view/DetailPage/Component/Comment.js
@@ -1,60 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "../DetailPage.scss";
 import StarRatingComponent from "react-star-rating-component";
 import { StarFilled, CloseOutlined, EditOutlined } from "@ant-design/icons";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as commentAction } from "../../../redux/moduels/comment";
+import EditInput from "./EditInput";
 
 const Comment = ({ data }) => {
   const dispatch = useDispatch();
   const userName = useSelector(state => state.user.name);
-
+  const [isEdit, setIsEdit] = useState(false);
   const deleteComment = () => {
     if (window.confirm("댓글을 삭제하시겠습니까?")) {
       dispatch(commentAction.deleteCommentDataOnDB(data.commentId));
     }
   };
+  const editComment = () => {
+    setIsEdit(true);
+  };
   return (
     <>
       <hr />
-      <div className='commentBorder'>
-        <div className='commentHeader'>
-          <span
-            className={`commentFont ${
-              userName && userName === data.userName && "myCommentFont"
-            }`}
-          >
-            {data.userName}
-          </span>
-          <div className='commentHeaderRight'>
-            <StarRatingComponent
-              name='rate'
-              editing={false}
-              renderStarIcon={() => (
-                <StarFilled
-                  style={{
-                    fontSize: "30px"
-                  }}
-                />
-              )}
-              starCount={5}
-              value={Number(data.userStar)}
-            />
-            {userName && userName === data.userName && (
-              <>
-                <EditOutlined className='commentIcon' />
-                <CloseOutlined
-                  className='commentIcon'
-                  onClick={deleteComment}
-                />
-              </>
-            )}
-          </div>
-        </div>
 
-        <p>{data.commentRegister.substr(0, 10)}</p>
-        <p className='commentFont'>{data.content}</p>
-      </div>
+      {isEdit ? (
+        <EditInput
+          commentId={data.commentId}
+          userStar={data.userStar}
+          content={data.content}
+        />
+      ) : (
+        <div className='commentBorder'>
+          <div className='commentHeader'>
+            <span
+              className={`commentFont ${
+                userName && userName === data.userName && "myCommentFont"
+              }`}
+            >
+              {data.userName}
+            </span>
+            <div className='commentHeaderRight'>
+              <StarRatingComponent
+                name='rate'
+                editing={false}
+                renderStarIcon={() => (
+                  <StarFilled
+                    style={{
+                      fontSize: "30px"
+                    }}
+                  />
+                )}
+                starCount={5}
+                value={Number(data.userStar)}
+              />
+              {userName && userName === data.userName && (
+                <>
+                  <EditOutlined className='commentIcon' onClick={editComment} />
+                  <CloseOutlined
+                    className='commentIcon'
+                    onClick={deleteComment}
+                  />
+                </>
+              )}
+            </div>
+          </div>
+
+          <p>{data.commentRegister.substr(0, 10)}</p>
+          <p className='commentFont'>{data.content}</p>
+        </div>
+      )}
     </>
   );
 };
